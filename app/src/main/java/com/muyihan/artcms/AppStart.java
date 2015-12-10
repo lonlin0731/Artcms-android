6a26d37ef2a549e6df9c77a0c1bf803414144829
lonfen1108 <huxiaolong930@163.com>
2015-12-10 10:22:04 +0800

    add AppStart

---
+++ app/src/main/java/com/muyihan/artcms/AppStart.java
@@ -0,0 +1,13 @@
+package com.muyihan.artcms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_start);
+    }
+}
